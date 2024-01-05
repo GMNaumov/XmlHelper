d138d750c986b1ebecfbbd3bd06ce1c5af944e95
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-05 21:11:02 +0300

    feat:Add Person class

--- ExampleSystemXml.cs
+++ ExampleSystemXml.cs
@@ -6,6 +6,8 @@ namespace XmlHelper
     {
         public void Run()
         {
+            List<Person> persons = new List<Person>();
+
             XmlDocument xDoc = new XmlDocument(); // Создаём новый объект для работы с файлом XML
             xDoc.Load("people.xml"); // Загружаем в созданный объект файл для работы с ним (файлом)
             XmlElement? xRoot = xDoc.DocumentElement; // Получаем корневой элемент XML-файла
@@ -13,22 +15,30 @@ namespace XmlHelper
             {
                 foreach (XmlElement xnode in xRoot) // Обрабатываем все дочерние элементы корневого узла
                 {
+                    Person person = new Person();
                     XmlNode? attr = xnode.Attributes.GetNamedItem("name"); // Получает атрибут следующего элемента по имени "name"
-                    Console.WriteLine(attr?.Value);
+                    person.Name = attr?.Value;
 
                     foreach (XmlNode childnode in xnode.ChildNodes) // Обрабатываем дочерние элементы
                     {
                         if (childnode.Name == "company") // Сравниваем имя элемента с company
                         {
-                            Console.WriteLine($"Company: {childnode.InnerText}");
+                            person.Company = childnode.InnerText;
                         }
 
                         if (childnode.Name == "age") // Сравниваем имя элемента с company
                         {
-                            Console.WriteLine($"Age: {childnode.InnerText}");
+                            person.Age = int.Parse(childnode.InnerText);
                         }
                     }
+
+                    persons.Add(person);
                 }
+            }
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine($"{person.Name}. Age:{person.Age}, company:{person.Company}");
             }
         }
     }
---
+++ Person.cs
@@ -0,0 +1,9 @@
+﻿namespace XmlHelper
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Company { get; set; }
+    }
+}
