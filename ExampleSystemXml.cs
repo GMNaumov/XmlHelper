b680b3ffaf853434933f8aad3cddd8fe8bd42e24
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-05 20:15:59 +0300

    feat: Added comments to lines

--- ExampleSystemXml.cs
+++ ExampleSystemXml.cs
@@ -6,24 +6,24 @@ namespace XmlHelper
     {
         public void Run()
         {
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.Load("people.xml");
-            XmlElement? xRoot = xDoc.DocumentElement;
+            XmlDocument xDoc = new XmlDocument(); // Создаём новый объект для работы с файлом XML
+            xDoc.Load("people.xml"); // Загружаем в созданный объект файл для работы с ним (файлом)
+            XmlElement? xRoot = xDoc.DocumentElement; // Получаем корневой элемент XML-файла
             if (xRoot != null)
             {
-                foreach (XmlElement xnode in xRoot)
+                foreach (XmlElement xnode in xRoot) // Обрабатываем все дочерние элементы корневого узла
                 {
-                    XmlNode? attr = xnode.Attributes.GetNamedItem("name");
+                    XmlNode? attr = xnode.Attributes.GetNamedItem("name"); // Получает атрибут следующего элемента по имени "name"
                     Console.WriteLine(attr?.Value);
 
-                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    foreach (XmlNode childnode in xnode.ChildNodes) // Обрабатываем дочерние элементы
                     {
-                        if (childnode.Name == "company")
+                        if (childnode.Name == "company") // Сравниваем имя элемента с company
                         {
                             Console.WriteLine($"Company: {childnode.InnerText}");
                         }
 
-                        if (childnode.Name == "age")
+                        if (childnode.Name == "age") // Сравниваем имя элемента с company
                         {
                             Console.WriteLine($"Age: {childnode.InnerText}");
                         }
